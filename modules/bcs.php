2ead6a40047f6c056b9a01455060ff7829a23df7
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-10 14:12:26 -0700

    se adecua como dijeron, se cambian fotos

--- modules/bcs.php
+++ modules/bcs.php
@@ -6,7 +6,7 @@
                 <div class="cont text-center">
                     <h1>Life Style & B.C.S</h1>
                     <div class="path">
-                        <a href="/">Inicio</a><span>/</span>Life Style & B.C.S
+                        <a href="/">Inicio</a><span>/</span>Baja California Sur
                     </div>
                 </div>
             </div>
@@ -19,14 +19,11 @@
 
 <!-- ==================== Start about ==================== -->
 
-<section class="about section-padding">
+<section class="about section-padding color-claro">
     <div class="container">
         <div class="row justify-content-center align-items-center">
-            <div class="col-lg-6">
-                <img src="/img/14.jpg" alt="">
-            </div>
-            <div class="col-lg-6">
-                <h2>East Cape</h2>
+            <div class="col-lg-12">
+                <h2 class="text-center">East Cape</h2>
                 <div class="txt-cont">
                     <p class="mb-20">
                         Observa el atardecer desde una playa desierta. Adéntrate en un bosque de gigantes cactus cardón.
@@ -45,81 +42,50 @@
 
 <!-- ==================== Start Services ==================== -->
 
-<section class="services halfbg">
-    <div class="background bg-img valign parallaxie" data-background="/img/12.jpg?=1" data-overlay-dark="1">
-    </div>
-    <div class="container ontop">
-        <div class="row">
-            <div class="col-lg-10 col-md-10 item-bx">
-                <h2 class="custom-font numb">Una increíble naturaleza</h2>
-                <p>Entre las playas más hermosas de México. Situado entre la Sierra de la Laguna y el Mar de Cortés.
-                    Cerca del Parque Natural de Cabo Pulmo y las cascadas y aguas termales naturales de Santiago.</p>
-                <a href="#contacto" class="btn-curve btn-color mt-30">
-                    <span>Contactar</span>
-                </a>
-            </div>
 
+<section class="about section-padding">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-6">
+                <img src="/img/07.jpg" alt="">
+            </div>
+            <div class="col-lg-6">
+                <h2>Una increíble naturaleza</h2>
+                <div class="txt-cont">
+                    <p class="mb-20">
+                        Entre las playas más hermosas de México. Situado entre la Sierra de la Laguna y el Mar de
+                        Cortés.
+                        Cerca del Parque Natural de Cabo Pulmo y las cascadas y aguas termales naturales de Santiago.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
-<!-- ==================== End Services ==================== -->
 
 
-<section class="team section-padding">
+
+<section class="about section-padding color-claro">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-5">
-                <div class="tit">
-                    <h2 class="wow flipInX" data-wow-delay=".5s">Una vida silvestre excepcional</h2>
-                    <p class="wow fadeInUp" data-wow-delay=".3s">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-6">
+                <h2>Una vida silvestre excepcional</h2>
+                <div class="txt-cont">
+                    <p class="mb-20">
                         Donde la biodiversidad de una tierra especial se encuentra con el acuario del mundo. Haz una
                         caminata entre una sorprendente diversidad de cactus endémicos. Observa una amplia variedad de
                         aves locales. Nada entre delfines y peces tropicales. Observa el anidamiento de tortugas por la
                         noche.
                     </p>
-                    <div class="navs mt-30 wow fadeInUp" data-wow-delay=".3s">
-                        <span class="prev">
-                            <i class="fas fa-chevron-left"></i>
-                        </span>
-                        <span class="next">
-                            <i class="fas fa-chevron-right"></i>
-                        </span>
-                    </div>
                 </div>
             </div>
-            <div class="col-lg-7">
-                <div class="team-container">
-                    <div class="item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="img wow imago">
-                            <img src="/img/team/7.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="img wow imago">
-                            <img src="/img/team/8.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="img wow imago">
-                            <img src="/img/team/9.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="img wow imago">
-                            <img src="/img/team/10.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item wow fadeInUp" data-wow-delay=".3s">
-                        <div class="img wow imago">
-                            <img src="/img/team/11.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-6">
+                <img src="/img/15.jpg" alt="">
             </div>
+
         </div>
     </div>
-    <div class="dots-half bg-img bg-repeat" data-background="/img/nalmia.png"></div>
 </section>
 
 
@@ -145,8 +111,7 @@
 </section>
 
 
-
-<section class="about section-padding">
+<section class="about section-padding color-claro">
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-6">
@@ -187,7 +152,7 @@
     </div>
 </section>
 
-<section class="about section-padding">
+<section class="about section-padding color-claro">
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-6">
