863e1777f7cf8d67e315a48ff7eb36b12fee2e0c
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-10 14:52:59 -0700

    se quita parallax, se arregla info, textos y colores de bg

--- modules/investment.php
+++ modules/investment.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="cont text-center">
-                    <h1>NALMIA Development</h1>
+                    <h1>Inversión</h1>
                     <div class="path">
                         <a href="/">Inicio</a><span>/</span>Información sobre la inversión y el mercado
                     </div>
@@ -23,8 +23,8 @@
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-10">
-                <h2>Inversión</h2>
-                <div class="txt-cont">
+                <h2 class="text-center">Inversión</h2>
+                <div class="txt-cont text-justify">
                     <p class="mb-20">
                         Vive en el paraíso mientras construyes riqueza. Los Barriles es uno de los destinos destacados
                         de Baja California Sur, ofreciendo la mejor oportunidad de inversión y los mayores retornos
@@ -38,26 +38,31 @@
 
 
 
-<section class="about mb-30">
+<section class="about mb-30 color-claro" style="padding: 2rem 0px;">
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-6">
-                <img style="width: 25rem;" src="/img/thf.svg" alt="">
+                <img style="width: 21rem;" src="/img/thf.svg" alt="">
             </div>
             <div class="col-lg-6">
                 <h2>Financia tu propiedad con Tu Hipoteca Fácil.</h2>
                 <div class="txt-cont">
                     <p class="mb-20">
                         Uno de los corredores de financiamiento inmobiliario más confiables de México. <br>
+                    </p>
+                    <a href="/villas" class="btn-curve btn-color mt-30">
+                        <span>Ver villas</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-12" style="margin-top: 3rem;">
+                <div class="container text-justify">
+                    <p>
                         Financia la compra de tu villa hasta un 80% del valor del préstamo. Los compradores de Estados
                         Unidos, la Unión Europea y Canadá son elegibles para el financiamiento, y el equipo de Nalmia se
                         asegurará de un proceso sin complicaciones. Contáctanos ahora para una revisión financiera sin
                         compromiso.
                     </p>
-
-                    <a href="/villas" class="btn-curve btn-color mt-30">
-                        <span>Ver villas</span>
-                    </a>
                 </div>
             </div>
         </div>
@@ -70,17 +75,6 @@
 
 <!-- ==================== Start Services ==================== -->
 
-<section class="services halfbg">
-    <div class="background bg-img valign parallaxie" data-background="/img/18.jpg?=1" data-overlay-dark="1">
-    </div>
-    <div class="container ontop">
-
-    </div>
-</section>
-
-
-
-
 
 <section class="about section-padding">
     <div class="container">
@@ -90,22 +84,25 @@
                 <div class="txt-cont">
                     <p class="mb-20">
                         Genera ingresos mensuales elevados a través de alquileres a corto plazo. <br>
-                        La demanda de alquiler en todo Baja California Sur sigue creciendo y las proyecciones futuras
-                        indican que la demanda aumentará. Gestiona tu alquiler desde tu teléfono móvil y deja que
-                        nuestro socio de administración de propiedades se encargue del resto.
                     </p>
                 </div>
             </div>
             <div class="col-lg-6">
                 <img src="/img/21.jpg" alt="">
             </div>
-
+            <div class="col-md-12" style="margin-top: 1.5rem;">
+                <div class="container text-justify">
+                    <p>La demanda de alquiler en todo Baja California Sur sigue creciendo y las proyecciones futuras
+                        indican que la demanda aumentará. Gestiona tu alquiler desde tu teléfono móvil y deja que
+                        nuestro socio de administración de propiedades se encargue del resto.</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
 
-<section class="about section-padding">
+<section class="about section-padding color-claro">
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-6">
@@ -116,14 +113,20 @@
                 <div class="txt-cont">
                     <p class="mb-20">
                         Contempla cómo el valor de tu propiedad se incrementa con el paso de los años. <br>
-                        El mercado inmobiliario de Baja California Sur se está expandiendo gracias a su atractiva
-                        ubicación, al flujo turístico y a un cambio hacia un entorno de mercado más estable. Disfruta de
-                        un promedio de apreciación anual mientras vives en el paraíso.
                     </p>
 
                     <a href="/villas" class="btn-curve btn-color mt-30">
                         <span>Ver villas</span>
                     </a>
+                </div>
+            </div>
+            <div class="col-md-12" style="margin-top: 1.5rem;">
+                <div class="container text-justify">
+                    <p>
+                        El mercado inmobiliario de Baja California Sur se está expandiendo gracias a su atractiva
+                        ubicación, al flujo turístico y a un cambio hacia un entorno de mercado más estable. Disfruta de
+                        un promedio de apreciación anual mientras vives en el paraíso.
+                    </p>
                 </div>
             </div>
         </div>
