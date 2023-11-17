ce463df072bf956d767a6177fa60d1ea80614160
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-11-17 11:09:09 -0700

    header ok, logo a la izq, logos centrados

--- includes/header.php
+++ includes/header.php
@@ -1,50 +1,59 @@
 <nav class="navbar change navbar-expand-lg">
     <div class="container-fluid">
-        <div class="d-none d-md-none d-lg-block col-md-4">
-            <!-- Logo -->
-            <a class="logo" href="/">
-                <img style="width: 13rem;" class="d-none d-md-block d-lg-block" src="/img/logo.png?=2" alt="logo">
-            </a>
-        </div>
-        <div class="col-4 d-block d-md-block d-lg-none">
-            <a class="logo" href="/">
-                <img style="width: 8rem;" class="d-block d-md-block d-lg-none" src="/img/logo.png?=2" alt="logo">
-            </a>
-        </div>
-        <div class="col-4 d-block d-md-block d-lg-none align-items-center" style="bottom: 13px;">
-            <div class="social-icon d-flex">
-                <a href="#0" style="border: 1px solid #d8c6b0"><i style="color: #d8c6b0;" class="fab fa-facebook-f"></i></a>
-                <a href="#0" style="border: 1px solid #d8c6b0"><i style="color: #d8c6b0;" class="fab fa-instagram"></i></a>
+        <div class="row w-100 justify-content-center align-items-center">
+            <div class="d-none d-md-none d-lg-block col-md-4">
+                <!-- Logo -->
+                <a class="logo" href="/">
+                    <img style="width: 13rem;" class="d-none d-md-block d-lg-block" src="/img/logo.png?=2" alt="logo">
+                </a>
+            </div>
+            <div class="col-2 d-block d-md-block d-lg-none" style="left: -3rem;">
+                <a class="logo" href="/">
+                    <img style="width: 8rem;" class="d-block d-md-block d-lg-none" src="/img/logo.png?=2" alt="logo">
+                </a>
+            </div>
+            <div class="col-2 d-block d-md-block d-lg-none"></div>
+            <div class="col-5 d-block d-md-block d-lg-none align-items-center" style="bottom: 13px;">
+                <div class="social-icon d-flex">
+                    <a href="#0" style="border: 1px solid #d8c6b0"><i style="color: #d8c6b0;"
+                            class="fab fa-facebook-f"></i></a>
+                    <a href="#0" style="border: 1px solid #d8c6b0"><i style="color: #d8c6b0;"
+                            class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <div class="col-2 d-block d-md-block d-lg-none align-items-center">
+            <button id="menu-movil" class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="icon-bar"><i class="fas fa-bars"></i></span>
+            </button>
+            </div>
+            <div class="col-12 col-md-12 col-lg-4" id="menu-items" style="left: 1rem;">
+                <!-- navbar links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/villas">Villas</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/bcs">B.C.S</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/investment">Inversionistas</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/contact">Contacto</a></li>
+                        <li class="d-block d-md-none">
+                            <a href="#0"><img style="width: 1.5rem;" src="/img/esp.png" alt=""></a>
+                            <a href="#0"><img style="width: 1.5rem;" src="/img/eng.png" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6 col-md-4 d-none  d-md-none d-lg-block text-right">
+                <div class="social-icon">
+                    <a href="#0"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#0"><i class="fab fa-instagram"></i></a>
+                    <a href="#0"><img style="width: 1.5rem;" src="/img/esp.png" alt=""></a>
+                    <a href="#0"><img style="width: 1.5rem;" src="/img/eng.png" alt=""></a>
+                </div>
             </div>
         </div>
 
-        <button id="menu-movil" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon-bar"><i class="fas fa-bars"></i></span>
-        </button>
-        <div class="col-12 col-md-4" id="menu-items">
-            <!-- navbar links -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/villas">Villas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/bcs">B.C.S</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/investment">Inversionistas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/contact">Contacto</a></li>
-                    <li class="d-block d-md-none">
-                        <a href="#0"><img style="width: 1.5rem;" src="/img/esp.png" alt=""></a>
-                        <a href="#0"><img style="width: 1.5rem;" src="/img/eng.png" alt=""></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-6 col-md-4 d-none  d-md-none d-lg-block text-right">
-            <div class="social-icon">
-                <a href="#0"><i class="fab fa-facebook-f"></i></a>
-                <a href="#0"><i class="fab fa-instagram"></i></a>
-                <a href="#0"><img style="width: 1.5rem;" src="/img/esp.png" alt=""></a>
-                <a href="#0"><img style="width: 1.5rem;" src="/img/eng.png" alt=""></a>
-            </div>
-        </div>
+
     </div>
 </nav>
