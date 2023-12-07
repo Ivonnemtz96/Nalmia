d47c632d9ff83005f33dff3c5a091230b7181895
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-07 16:54:40 -0700

    se copian a inglés

---
+++ modules/en/form-paginas.php
@@ -0,0 +1,50 @@
+<section class="contact" id="contact">
+    <div class="info bg-gray pt-80 pb-80" style="background:#fff;">
+        <div class="container">
+            <h6 class="sub-title">Contact Us</h6>
+            <h2 class="mb-20 playfont">Learn more about NALMIA</h2>
+            <p>
+                Are you ready to learn more about NALMIA Villas? We would be delighted to assist you. Please complete
+                the form below, and a representative will be in touch with you shortly.
+            </p>
+            <div class="row contacto-index" style="margin-bottom: -2rem;">
+                <div class="col-md-6">
+                    <img src="/img/50.jpg" alt="" style="margin-bottom: 2rem;">
+                </div>
+                <div class="col-lg-6 form" style="padding: 0;">
+                    <form  method="post" action="/php/contact.php">
+                        <div class="messages"></div>
+                        <div class="controls">
+                            <div class="form-group">
+                                <input id="form_name" type="text" name="nombre" placeholder="Name * "
+                                    required="required">
+                            </div>
+
+                            <div class="form-group">
+                                <input id="form_email" type="email" name="mail" placeholder="Email *"
+                                    required="required">
+                            </div>
+
+                            <div class="form-group">
+                                <input id="form_name" type="text" name="tel" placeholder="Phone number *"
+                                    required="required">
+                            </div>
+
+                            <div class="form-group">
+                                <textarea id="form_message" name="msj" placeholder="Message * " rows="4"
+                                    required="required"></textarea>
+                            </div>
+                            <div class="row " style="margin-bottom: 3rem; margin-top:3rem;">
+                                <div class="col-12 col-md-12">
+                                    <div class="g-recaptcha" data-sitekey="6Lf92OAfAAAAAEy9m8sf2kbU0ojkdDs5CNOnaNYS"
+                                        required></div>
+                                </div>
+                            </div>
+                            <button type="submit" name="submit" class="btn-curve btn-color"><span>Send</span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ modules/en/index.php
@@ -0,0 +1,329 @@
+<header class="slider text-center">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="bg-img valign" style="height: 55rem; background-position: bottom;"
+                    data-background="/img/slid/9.jpg?=1" data-overlay-dark="1">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7 col-md-9">
+                                <div class="caption center  animate__animated animate__fadeInUp"
+                                    style="margin-top: 4rem;">
+                                    <h1 data-splitting class="playfont" style="text-shadow:  10px 10px 10px #000; ">
+                                        <img class="" src="/img/logo.png?=2" alt="Nalmia development">
+                                    </h1>
+                                    <a href="/villas" class="btn-curve btn-color mt-20">
+                                        <span>View Villas</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- slider setting -->
+        <div hidden class="setone setwo">
+            <div class="swiper-button-next swiper-nav-ctrl next-ctrl cursor-pointer">
+                <i class="fas fa-chevron-right"></i>
+            </div>
+            <div class="swiper-button-prev swiper-nav-ctrl prev-ctrl cursor-pointer">
+                <i class="fas fa-chevron-left"></i>
+            </div>
+        </div>
+
+        <div class="social-icon">
+            <a href="#0"><i class="fab fa-facebook-f"></i></a>
+            <a href="#0"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+</header>
+
+<!-- ==================== End Slider ==================== -->
+
+<section class="about section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 valign">
+                <div class="exp-content nopat wow fadeInUp" data-wow-delay=".3s">
+                    <!-- <h6 class="sub-title">About Us</h6> -->
+                    <h2 class="mb-20 playfont" style="font-size: 3rem;">NALMIA</h2>
+                    <p>
+                        High-end Real Estate Developer. <br>
+                        Building the future of Baja California Sur.
+                    </p> <br><br><br>
+                    <a href="#contact" class="btn-curve btn-color mt-30">
+                        <span>Contact Us</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="ab-exp">
+                    <div class="row">
+                        <div class="col-md-4 mb-20">
+
+                        </div>
+                        <div class="col-md-8 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="img mb-20 wow imago">
+                                <img src="/img/06.jpg?=1" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-7 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="img wow imago">
+                                <img src="/img/05.jpg?=1" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="img wow imago d-none d-md-block">
+                                <img src="/img/04.jpg?=1" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- ==================== Start about ==================== -->
+
+<section class="about section-padding beige">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="extra wow fadeInUp" data-wow-delay=".3s">
+                    <h6 class="sub-title">Innovative Designs</h6>
+                    <h2 class="mb-20 playfont" style="margin-bottom: 2rem;">Villas NALMIA </h2>
+                    <p style="margin-bottom: 2rem;">
+                        Breathtaking designs compliment the raw beauty of Los Barriles and it’s surroundings.
+                        <br>
+                        A modern interpretation of traditional aesthetics.
+                    </p>
+                    <a href="/villas" style="margin-bottom: 1rem;" class="btn-curve btn-color mt-30">
+                        <span>View Villas</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 valign  d-block d-md-none">
+                <div class="specialty wow fadeInUp" data-wow-delay=".5s">
+                    <img src="/img/09.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+    <div class="bg-img pat valign text-center" data-background="/img/32.jpg">
+        <!-- <img src="/img/02.jpg" alt="" srcset=""> -->
+
+    </div>
+</section>
+
+
+<section class="parallax-section">
+    <div class="block">
+        <img src="/img/08.jpg?=1" data-speed="-1" class="img-parallax">
+        <div class="exp-content wow fadeInUp" data-wow-delay=".3s">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-12">
+                        <h2 class="playfont" style="margin-bottom: 4rem; text-shadow: 1px 1px 1px #000;">East Cape, a
+                            living paradise</h2>
+                        <p style="margin-bottom: .5rem; text-shadow: 1px 1px 1px #000;">
+                            Live peace and freedom in an untouched nature. <br> Experiment a unique and healthy
+                            lifestyle.
+                        </p>
+                        <a href="/bcs" class="btn-curve btn-color mt-30 btn-explorar">
+                            <span>Explore</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- ==================== End about ==================== -->
+
+<section class="about section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="ab-exp">
+                    <div class="row">
+                        <div class="col-md-8 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="img mb-20 wow imago">
+                                <img src="/img/34.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-20">
+
+                        </div>
+
+                        <div class="col-md-7 wow fadeInUp" data-wow-delay=".3s">
+                            <div class="img wow imago">
+                                <img src="/img/33.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="img wow imago d-none d-md-block">
+                                <img src="/img/35.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-lg-5 valign">
+                <div class="exp-content nopat wow fadeInUp" data-wow-delay=".3s">
+                    <h6 class="sub-title">A profitable investment</h6>
+                    <h2 class="mb-20 playfont">NALMIA Development</h2>
+                    <p>
+                        An opportunity to create a self-sustaining investment which generates high income monthly.
+
+                    </p> <br><br><br>
+                    <a href="/bcs" class="btn-curve btn-color mt-30">
+                        <span>Explore</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="work-carousel metro section-padding dark">
+    <div class="container-fluid d-none d-md-block">
+        <div class="container">
+            <div class="section-head text-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-8 col-sm-10">
+                        <h4 class="playfont wow flipInX" data-wow-delay=".5s" style="color: #000;">Our Gallery</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 no-padding">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" style="background-position: center;" data-background="/img/38.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" style="background-position: center;" data-background="/img/36.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" style="background-position: center;" data-background="/img/39.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" style="background-position: center;" data-background="/img/37.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" style="background-position: center;" data-background="/img/40.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- slider setting -->
+                    <div class="swiper-button-next swiper-nav-ctrl next-ctrl cursor-pointer">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                    <div class="swiper-button-prev swiper-nav-ctrl prev-ctrl cursor-pointer">
+                        <i class="fas fa-chevron-left"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="work-carousel metro section-padding dark  d-block d-md-none">
+    <div class="container-fluid d-block d-md-none">
+        <div class="container">
+            <div class="section-head text-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-8 col-sm-10">
+                        <h4 class="playfont wow flipInX" data-wow-delay=".5s" style="color: #000;">Our Gallery</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 no-padding">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" data-background="/img/43.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" data-background="/img/41.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" data-background="/img/44.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" data-background="/img/42.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="content wow fadeInUp" data-wow-delay=".3s">
+                                <div class="item-img bg-img wow imago" data-background="/img/45.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- slider setting -->
+                    <div class="swiper-button-next swiper-nav-ctrl next-ctrl cursor-pointer">
+                        <i class="fas fa-chevron-right"></i>
+                    </div>
+                    <div class="swiper-button-prev swiper-nav-ctrl prev-ctrl cursor-pointer">
+                        <i class="fas fa-chevron-left"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
